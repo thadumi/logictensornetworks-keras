94af9ff5add3a9b8c872eb8897a68d20b7b8f2a7
thadumi <dumitrescu_theodor_a_@hotmail.it>
2019-11-14 10:13:50 +0100

    tmp

--- logictensornetworks/keras/topology.py
+++ logictensornetworks/keras/topology.py
@@ -5,87 +5,19 @@
 
 import tensorflow as tf
 from tensorflow import keras as K
+from tensorflow.keras import backend
 
 from logictensornetworks import backend as be
 
-BIAS_factor = 1e-7
-BIAS = tf.Variable(0.0, trainable=False)
 
-'''
-x = K.Input(shape=(32,))
-
-CONSTANTS = {}
-VARIABLE = {}
-
-
-def constant(label, shape):
-    CONSTANTS[label] = shape
-
-
-def variable(shape, label):
-    VARIABLE[label] = shape
-
-
-size = 20
-for l in 'abcdefghijklmn':
-    constant(l, size)
-
-for l in 'pq':
-    variable((14, 20), l)
-
-for l in ('p1', 'q1'):
-    variable((8, 20), l)
-
-for l in ('p2', 'q2'):
-    variable((6, 20), l)
-
-
-def total_shape_constants():
-    return tuple(CONSTANTS.values())
-
-
-print(len(total_shape_constants()))
-'''
-
-
-def _variable(label, features=None, tensor=None, const=None):
-    """
-
-    :param label: the name of the tensor
-    :param features: a scalar representing the number of features of the variable has to be passed if a tensor is not specified
-                     Will create a K.Input tensor of the shape (None, features)
-    :param tensor: a tf.Tensor which represent initial value of the variable
-    :return: a tensor representing the variable
-    """
-    if features is not None and type(features) is int:
-        var = K.Input(dtype=tf.float32, shape=(None, features), name=label)
-    elif tensor is not None and tf.is_tensor(tensor):
-        var = tf.identity(tensor, name=label)
-    else:
-        var = tf.constant(const, name=label)
-
-    var.doms = [label]
-    return var
-
-def constant(label, value=None, min_value=None, max_value=None):
-    if value is not None:
-        const = tf.constant(value, name=label)
-    else:
-        const = tf.constant(tf.random.uniform(
-            shape=(1, len(min_value)),
-            minval=min_value,
-            maxval=max_value),
-            name=label)
-
-    const.doms = []
-    return const
+# BIAS_factor = 1e-7
+# BIAS = tf.Variable(0.0, trainable=False)
 
 
 class Predicate(K.layers.Layer):
-
     def __init__(self,
                  label,
-                 number_of_features_or_vars=None,
+                 number_of_features=None,
                  pred_definition=None,
                  layers=None,
                  *kwargs):
@@ -97,12 +29,12 @@ class Predicate(K.layers.Layer):
         self.pred_definition = pred_definition
         self.layers = layers or 4
 
-        if type(number_of_features_or_vars) is list:
-            self.number_of_features = sum([int(v.shape[1]) for v in number_of_features_or_vars])
-        elif tf.is_tensor(number_of_features_or_vars):
-            self.number_of_features = int(number_of_features_or_vars.shape[1])
+        if type(number_of_features) is list:
+            self.number_of_features = sum([int(v.shape[1]) for v in number_of_features])
+        elif tf.is_tensor(number_of_features):
+            self.number_of_features = int(number_of_features.shape[1])
         else:
-            self.number_of_features = number_of_features_or_vars
+            self.number_of_features = number_of_features
 
         assert type(self.number_of_features) is int
 
@@ -110,22 +42,25 @@ class Predicate(K.layers.Layer):
         self.w = None
         self.u = None
 
-    def build(self):
+    def build(self, inputs):
         # TODO: check if is passing a layer or a
+        # https://github.com/keras-team/keras/issues/8131
 
         if self.pred_definition is None:
             # if there is not a custom predicate model defined create one using the default structure
-
             self.w = self.add_weight(name=self.label + "_W",
                                      shape=(self.layers, self.number_of_features + 1, self.number_of_features + 1),
-                                     initializer=K.initializers.RandomNormal(mean=0, stddev=1, seed=None))
+                                     initializer=K.initializers.RandomNormal(mean=0, stddev=1, seed=None),
+                                     trainable=True)
 
             self.u = self.add_weight(name=self.label + "_u",
                                      shape=(self.layers, 1),
-                                     initializer=K.initializers.Ones())
+                                     initializer=K.initializers.Ones(),
+                                     trainable=True)
+
         super(Predicate, self).build(True)
 
-    def call(self, inputs):
+    def call(self, *inputs):
         crossed_args, list_of_args_in_crossed_args = be.cross_args(inputs)
         result = self._call_default_model(*list_of_args_in_crossed_args)
 
@@ -135,10 +70,10 @@ class Predicate(K.layers.Layer):
             result = tf.reshape(result, (1,))
         result.doms = crossed_args.doms
 
-        BIAS.assign(tf.divide(BIAS + .5 - tf.reduce_mean(result), 2) * BIAS_factor)
+        # BIAS.assign(tf.divide(BIAS + .5 - tf.reduce_mean(result), 2) * BIAS_factor)
         return result
 
-    def _call_default_model(self, inputs):
+    def _call_default_model(self, *inputs):
         app_label = self.label + "/" + "_".join([arg.name.split(":")[0] for arg in inputs]) + "/"
         tensor_args = tf.concat(inputs, axis=1)
 
@@ -153,15 +88,31 @@ class Predicate(K.layers.Layer):
         return tf.sigmoid(gX, name=app_label)
 
 
-'''
-g1 = {l: constant(l, min_value=[0.] * 20, max_value=[1.] * 20) for l in 'abcdefgh'}
-g2 = {l: constant(l, min_value=[0.] * 20, max_value=[1.] * 20) for l in 'ijklmn'}
-g = {**g1, **g2}
+# K.Input(shape=shape, name='constant_' + label)
+def constant(label, shape):
+    c = tf.Variable()
+    c.doms = []
+
+def variable(label, shape):
+    pass
+
+def predicate(label, features):
+    return lambda *inputs: Predicate(label, number_of_features=features)(*inputs)
+
+
+embedding_size = 10  # each constant is interperted in a vector of this size
+
+# create on constant for each individual a,b,... i,j, ...
+constants = {l: constant(l, embedding_size) for l in 'abcdefghijklmn'}
 
 friends = [('a', 'b'), ('a', 'e'), ('a', 'f'), ('a', 'g'), ('b', 'c'), ('c', 'd'), ('e', 'f'), ('g', 'h'),
            ('i', 'j'), ('j', 'm'), ('k', 'l'), ('m', 'n')]
-smokes = ['a', 'e', 'f', 'g', 'j', 'n']
-cancer = ['a', 'e']
 
-p = _variable('p', tf.concat(list(g.values()), axis=0))
-'''
+Friends = predicate('Friends', embedding_size * 2)
+Smokers = predicate('Smokers', embedding_size)
+Cancer = predicate('Cancer', embedding_size)
+
+facts = [Friends(constants[x], constants[y]) for (x, y) in friends]
+out = K.layers.Concatenate()(facts)
+
+model = K.Model(inputs=constants, outputs=out)
